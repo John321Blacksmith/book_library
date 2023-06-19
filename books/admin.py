1bc54d62a4a73c3f986149fcdbc46fb55038f519
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-19 15:27:38 +0300

    some improvements

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Magazine, DVD, CD
 
 # Register your models here.
 
 
 admin.site.register(Book)
+admin.site.register(Magazine)
+admin.site.register(DVD)
+admin.site.register(CD)
--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
-
+from .models import Contributor, Book, Magazine, DVD, CD
 # Create your tests here.
+
+
+class LibraryModelsDataTest(TestCase):
+
+	@classmethod
+	def setUpTestData(cls):
+		# create several contributors
+		cls.contributor1 = Contributor.objects.create('Frank F.')
+		cls.contributor2 = Contributor.objects.create('John K.')
+		cls.contributor3 = Contributor.objects.create('Paul V.')
+		cls.contributor5 = Contributor.objects.create('Will M.')
+		cls.contributor6 = Contributor.objects.create('Will M.')
+		cls.contributor7 = Contributor.objects.create('Will M.')
+
+		# create several library items
+		cls.book = Book.objects.create(
+					title='Test Book',
+					upc='t111',
+					subject='testing',
+					isbn='111222333444',
+					dds='55.66T',
+					contributors=cls.contributor1,
+			)
+		cls.magazine = Magazine.objects.create(
+					title='Test Magazine',
+					upc='t222',
+					subject='testing',
+					issue_num='#555',
+					volume_num='5',
+					contributors=cls.contributor2,
+			)
+		cls.dvd = DVD.objects.create(
+					title='Test dvd',
+					upc='t333',
+					subject='testing',
+					genre='test',
+					contributors=cls.contributor3,
+			)
+		cls.cd = CD.objects.create(
+					title='Test cd',
+					upc='t444',
+					subject='testing',
+					contributors=cls.contributor4,
+			)
+
+
+	def test_contributors_exist(self):
+		self.assertIn(self.contributor1, Contributor.objects.all())
+		self.assertIn(self.contributor2, Contributor.objects.all())
+		self.assertIn(self.contributor3, Contributor.objects.all())
+		self.assertIn(self.contributor5, Contributor.objects.all())
+		self.assertIn(self.contributor6, Contributor.objects.all())
+		self.assertIn(self.contributor7, Contributor.objects.all())
+
+
